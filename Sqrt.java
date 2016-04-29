79533b4034b632411c485cabe4c57a6fc4520e4c
Jeff Qian <jifeiqian@gmail.com>
2016-04-29 15:57:12 -0700

    update double version
    leetcode 69. Sqrt(x)

--- Sqrt.java
+++ Sqrt.java
@@ -6,7 +6,7 @@
 public Solution {
   public int mySqrt(int x) {
     if (x <= 0) return 0;
-    int start = 1;
+    int start = 0;
     int end = x;
     while (start + 1 < end) {
       int mid = start + (end - start) / 2;
@@ -26,3 +26,30 @@ public Solution {
     }
   }
 }
+
+/**
+ * second version is double version 
+ * end - start > tolerance
+ * x less thant 1 then end pointer starts from 1
+**/
+public Solution{
+  public double mySqrt(double x) {
+      if (x <= 0) return 0;
+      double lo = 0;
+      double high = x < 1 ? 1 : x;
+      double tolerance = 1e-9;
+      while (lo <= high && (high - lo) > tolerance) {
+          int mid = lo + (high - lo) / 2;
+          if (mid < x / mid) {
+            lo = mid;
+          } else if (mid > x / mid) {
+            high = mid;
+          } else {
+            return mid;
+          }
+      }
+      
+      return lo + (high - lo) / 2;
+  }
+}
+
