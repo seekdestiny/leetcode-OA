20e3b22307e9d0368f0515213d79cf68bcc66d2d
Jeff Qian <jifeiqian@gmail.com>
2016-05-02 21:29:01 -0700

    Update merge_k_sorted_lists.java

--- merge_k_sorted_lists.java
+++ merge_k_sorted_lists.java
@@ -1,56 +1,60 @@
 /**
  * solution1: use min heap
 **/
-
+/**
+* Definition for ListNode.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int val) {
+ *         this.val = val;
+ *         this.next = null;
+ *     }
+ * }
+ */ 
 public class Solution {
     /**
-     * @param arrays k sorted integer arrays
-     * @return a sorted array
+     * @param lists: a list of ListNode
+     * @return: The head of one sorted list.
      */
-    class Element {
-        int row;
-        int col;
-        int value;
-        
-        public Element(int row, int col, int value) {
-            this.row = row;
-            this.col = col;
-            this.value = value;
-        }
-    }
-     
-    private Comparator<Element> ElementComparator = new Comparator<Element>() {
-        public int compare(Element e1, Element e2) {
-            return e1.value - e2.value;
+    private Comparator<ListNode> ListNodeComparator = new Comparator<ListNode>() {
+        public int compare(ListNode l1, ListNode l2) {
+            if (l1 == null) {
+                return 1;
+            } else if (l2 == null) {
+                return -1;
+            } else {
+                return l1.val - l2.val;
+            }
         }
     };
      
-    public List<Integer> mergekSortedArrays(int[][] arrays) {
-        // Write your code here
-        List<Integer> res = new ArrayList<Integer>();
-        if (arrays == null || arrays.length == 0) {
-            return res;
+    public ListNode mergeKLists(List<ListNode> lists) {  
+        // write your code here
+        if (lists == null || lists.size() == 0) {
+            return null;
         }
-
-        Queue<Element> q = new PriorityQueue<Element>(arrays.length, ElementComparator);
         
-        for (int i = 0; i < arrays.length; i++) {
-            if (arrays[i].length > 0) {
-                q.add(new Element(i, 0, arrays[i][0]));
+        Queue<ListNode> heap = new PriorityQueue<ListNode>(lists.size(), ListNodeComparator);
+        
+        for (int i = 0; i < lists.size(); i++) {
+            if (lists.get(i) != null) {
+                heap.add(lists.get(i));
             }
         }
         
-        while (!q.isEmpty()) {
-            Element temp = q.poll();
-            res.add(temp.value);
-            int row = temp.row;
-            int col = temp.col;
-            if (col < arrays[row].length - 1) {
-                col++;
-                q.add(new Element(row, col, arrays[row][col]));
+        ListNode dummy = new ListNode(0);
+        ListNode track = dummy;
+        
+        while (!heap.isEmpty()) {
+            ListNode temp = heap.poll();
+            track.next = temp;
+            track = track.next;
+            if (temp.next != null) {
+                heap.add(temp.next);
             }
         }
         
-        return res;
+        return dummy.next;
     }
 }
