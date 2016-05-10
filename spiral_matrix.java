84e5cadfdc3d4af9946567b13e069b010ec329fe
Jeff Qian <jifeiqian@gmail.com>
2016-05-10 10:15:31 -0700

    add better solution

--- spiral_matrix.java
+++ spiral_matrix.java
@@ -65,3 +65,40 @@ public class Solution {
         return res;
     }
 }
+
+/**
+ * 与Rotate Image那题类似，一层一层处理。但这题有两个注意点：
+ * 1. m和n可以不同，所以对于第i层来说，最后一行为lastRow = m-1-i，而最后一列为lastCol = n-1-i。所以层数由min(m,n)决定。
+ * 2. 当min(m,n)为奇数时，最后一层为一行或一列，需要特殊处理。
+**/
+public class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> res = new ArrayList<Integer>();
+        
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return res;
+        }
+        
+        int m = matrix.length;
+        int n = matrix[0].length;
+        
+        int level = (Math.min(m, n) + 1) / 2;
+        for (int i = 0; i < level; i++) {
+            int lastRow = m - 1 - i;
+            int lastCol = n - 1 - i;
+            
+            if (lastRow == i) {
+                for (int j = i; j <= lastCol; j++) res.add(matrix[i][j]);
+            } else if (lastCol == i) {
+                for (int j = i; j <= lastRow; j++) res.add(matrix[j][i]);
+            } else {
+                for (int j = i; j < lastCol; j++) res.add(matrix[i][j]);
+                for (int j = i; j < lastRow; j++) res.add(matrix[j][lastCol]);
+                for (int j = lastCol; j > i; j--) res.add(matrix[lastRow][j]);
+                for (int j = lastRow; j > i; j--) res.add(matrix[j][i]);
+            }
+        }
+        
+        return res;
+    }
+}
