5fb51c1263ae13f21b65d76cfe4dfaa6fa3d6e61
Jeff Qian <jifeiqian@gmail.com>
2016-05-18 13:40:22 -0700

    Update course_schedule.java

--- course_schedule.java
+++ course_schedule.java
@@ -42,3 +42,54 @@ public class Solution {
         return count == 0;
     }
 }
+
+/** dfs
+ *  each node has three status
+ *  0: not visited
+ *  1: searching
+ *  2: visited
+ **/
+ 
+ public class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        if (prerequisites == null || prerequisites.length == 0 || prerequisites[0].length == 0) {
+            return true;
+        }
+        
+        Map<Integer, List<Integer>> graph = new HashMap<Integer, List<Integer>>();
+
+        for (int i = 0; i < numCourses; i++) {
+            graph.put(i, new ArrayList<Integer>());
+        }
+        
+        for (int i = 0; i < prerequisites.length; i++) {
+            graph.get(prerequisites[i][1]).add(prerequisites[i][0]);
+        }
+        
+        int[] visited = new int[numCourses];
+        for (int i = 0; i < numCourses; i++) {
+            if (visited[i] == 0 && hasCycle(i, visited, graph)) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+    
+    private boolean hasCycle(int i, int[] visited, Map<Integer, List<Integer>> graph) {
+        
+        visited[i] = 1;
+        for (Integer v1 : graph.get(i)) {
+            if (visited[v1] == 1) {
+                return true;
+            }
+            
+            if (visited[v1] == 0 && hasCycle(v1, visited, graph)) {
+                return true;
+            }
+        }
+        
+        visited[i] = 2;
+        return false;
+    }
+}
