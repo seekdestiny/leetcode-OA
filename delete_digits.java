33f2d6026fe39b736ad220c7d48ef5e6d476be05
Jeff Qian <jifeiqian@gmail.com>
2016-06-10 14:26:20 -0700

    Update delete_digits.java

--- delete_digits.java
+++ delete_digits.java
@@ -1,5 +1,6 @@
 //k rounds scan
-..every round delete the first number which is larger than the successor
+//every round delete the first number which is larger than the successor
+//O(k * n)
 
 public class Solution {
     /**
@@ -28,5 +29,43 @@ public class Solution {
         }
         
         return A.substring(i, A.length());
+    }
+}
+
+//increasing stack
+//O(n) 
+
+public class Solution {
+    /**
+     *@param A: A positive integer which has N digits, A is a string.
+     *@param k: Remove k digits.
+     *@return: A string
+     */
+    public String DeleteDigits(String A, int k) {
+        // write your code here
+        if (A.length() < k) return "";
+        
+        StringBuilder sb = new StringBuilder();
+        
+        int cnt = 0;
+        for (int i = 0; i < A.length(); i++) {
+            while (sb.length() != 0 && 
+                   sb.charAt(sb.length() - 1) > A.charAt(i) && 
+                   cnt < k) {
+                sb.deleteCharAt(sb.length() - 1);
+                cnt++;
+            }
+            
+            if (A.charAt(i) != '0' || sb.length() != 0) {
+                sb.append(A.charAt(i));
+            }
+        }
+        
+        while (cnt < k) {
+            sb.deleteCharAt(sb.length() - 1);
+            cnt++;
+        }
+        
+        return sb.toString();
     }
 }
