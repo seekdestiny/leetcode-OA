d1aa67a27ea89e996fa535cdfd2168192f933884
Jeff Qian <jifeiqian@gmail.com>
2016-06-15 15:34:35 -0700

    lintcode - 508

---
+++ wiggle_sort.java
@@ -0,0 +1,20 @@
+public class Solution {
+    /**
+     * @param nums a list of integer
+     * @return void
+     */
+    public void wiggleSort(int[] nums) {
+        // Write your code here
+        if (nums == null || nums.length == 0) {
+            return;
+        }
+        
+        for (int i = 1; i < nums.length; i++) {
+            if ((i % 2 == 1 && nums[i] < nums[i - 1]) || (i % 2 == 0 && nums[i] > nums[i - 1])) {
+                int temp = nums[i];
+                nums[i] = nums[i - 1];
+                nums[i - 1] = temp;
+            }
+        }
+    }
+}
