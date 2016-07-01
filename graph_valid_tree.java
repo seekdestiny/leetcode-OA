f7f213b9c6dcb49a605b60d9db520394cde9d945
Jeff Qian <jifeiqian@gmail.com>
2016-07-01 15:21:49 -0700

    Update graph_valid_tree.java

--- graph_valid_tree.java
+++ graph_valid_tree.java
@@ -43,6 +43,51 @@ public class Solution {
         return cnt == 0;
     }
 }
+///////////////////
+public class Solution {
+    /**
+     * @param n an integer
+     * @param edges a list of undirected edges
+     * @return true if it's a valid tree, or false
+     */
+    public boolean validTree(int n, int[][] edges) {
+        // Write your code here
+        Map<Integer, Set<Integer>> graph = new HashMap<Integer, Set<Integer>>();
+        
+        for (int i = 0; i < n; i++) {
+            graph.put(i, new HashSet<Integer>());
+        }
+        
+        for (int i = 0; i < edges.length; i++) {
+            graph.get(edges[i][0]).add(edges[i][1]);      
+            graph.get(edges[i][1]).add(edges[i][0]);
+        }
+        
+        Queue<Integer> queue = new LinkedList<Integer>();
+        Set<Integer> set = new HashSet<Integer>();
+        
+        queue.add(0);
+        
+        int cnt = n;
+        while (!queue.isEmpty()) {
+            int temp = queue.poll();
+            if (set.contains(temp)) {
+                return false;
+            }
+            
+            set.add(temp);
+            
+            for (int item: graph.get(temp)) {
+                if (!set.contains(item)) {
+                    queue.add(item);
+                }
+            }
+            cnt--;
+        }
+        
+        return cnt == 0;
+    }
+}
 
 //method2: dfs
 public class Solution {
