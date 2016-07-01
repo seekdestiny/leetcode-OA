7e2a39a2ec027a8a5e4730a599e7037d784987a5
Jeff Qian <jifeiqian@gmail.com>
2016-07-01 15:04:54 -0700

    Update graph_valid_tree.java

--- graph_valid_tree.java
+++ graph_valid_tree.java
@@ -43,3 +43,56 @@ public class Solution {
         return cnt == 0;
     }
 }
+
+//method2: dfs
+public class Solution {
+    /**
+     * @param n an integer
+     * @param edges a list of undirected edges
+     * @return true if it's a valid tree, or false
+     */
+    public boolean validTree(int n, int[][] edges) {
+        // Write your code here
+        Map<Integer, Set<Integer>> graph = new HashMap<Integer, Set<Integer>>();
+        
+        for (int i = 0; i < n; i++) {
+            graph.put(i, new HashSet<Integer>());
+        }
+        
+        for (int i = 0; i < edges.length; i++) {
+            graph.get(edges[i][0]).add(edges[i][1]);      
+            graph.get(edges[i][1]).add(edges[i][0]);
+        }
+        
+        boolean[] visited = new boolean[n];
+        
+        if (!dfs(0, -1, graph, visited)) {
+            return false;
+        }
+        
+        for (boolean v : visited) {
+            if (!v) {
+                return false;
+            }
+        }
+       
+        return true;
+    }
+    
+    private boolean dfs(int i, int prev, Map<Integer, Set<Integer>> graph, 
+                        boolean[] visited) {
+        if (visited[i]) {
+            return false;
+        }
+        
+        visited[i] = true;;
+        
+        for (int v : graph.get(i)) {
+            if (v != prev && !dfs(v, i, graph, visited)) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
